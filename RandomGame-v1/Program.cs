922f7d629d39fb6c8ebe348abe09d67368bb57e4
Pini4 <User@WIN-GA1Q70LI9LP>
2022-09-15 10:49:46 +0300

    Добавьте файлы проекта.

---
+++ RandomGame-v1/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int a = rand.Next(51);
+        int count = 1;
+
+        Console.WriteLine("Компьютер загадал число от 0 до 50. У вас есть очень много попыток, чтобы отгадать число:D.");
+        Console.WriteLine("Введите первое число:");
+
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        while (count <= 1000000)
+        {
+            if (a == b)
+            {
+                Console.WriteLine($"Поздровляю вы угадали число с {count} попытки!");
+                break;
+            }
+            else
+            {
+                count++;
+
+                Console.WriteLine($"Вы не угадали число. У вас есть еще попытка :D");
+                b = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+
+
+
+        }
+    }
+}
